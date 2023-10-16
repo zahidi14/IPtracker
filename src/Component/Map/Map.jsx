649a17eb26651296fe8885b2417d7716f0d11728
zahidi14 <d.zahidi1412@gmail.com>
2023-10-16 21:53:37 +0800

    finish

--- src/Component/Map/Map.jsx
+++ src/Component/Map/Map.jsx
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from "react";
 import "./map.scss";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 
 const Map = ({ cordinate }) => {
   console.log("kordinat", cordinate);
+  const [position, setPosition] = useState(cordinate);
+  // useEffect(() => {
+  //   if (cordinate && cordinate.latlng) {
+  //     setPosition(cordinate.latlng); // update via props
+  //   }
+  // }, [cordinate]);
+  function ChangeMapView({ coords }) {
+    const map = useMap();
+    map.setView(coords, map.getZoom());
+
+    return null;
+  }
   return (
     <div className="map-container">
-      <MapContainer center={cordinate} zoom={3} scrollWheelZoom={false}>
+      <MapContainer center={cordinate} zoom={12} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <ChangeMapView coords={cordinate} />
         <Marker position={cordinate}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
